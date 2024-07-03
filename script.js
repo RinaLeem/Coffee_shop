a7a173748204e041ecd238a91a60339a44a74364
Екатерина Балашова <rinaleem@mail.ru>
2024-07-03 22:10:05 +0400

    Updated files

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ new Swiper(".swiper", {
   spaceBetween: 30,
   limitToOriginalSize: true,
 });
+
+const callback = () => {
+  alert("Hi!");
+};
+
+const redirect = () => {
+  alert("Bye!");
+};
+const button = document.getElementsByClassName("button");
+button.addEventListener("click", callback);
+
+const button_redirect = document.getElementsByClassName("contact__link-button");
+button.addEventListener("click", redirect);
